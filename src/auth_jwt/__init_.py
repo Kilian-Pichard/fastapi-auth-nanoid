6ba9d8febc0c3f3f3836c9572c8172d9ff5a2bea
Kilian-Pichard <kilian.pichard@proton.me>
2024-03-02 22:07:28 +0100

    Creation of the FastAPI JWT Auth source code
    The source code is based on the IndominusByte FastAPI JWT Auth project

---
+++ src/auth_jwt/__init_.py
@@ -0,0 +1,6 @@
+"""A FastAPI extension that provides JWT Auth support using NanoID (secure, easy to use and lightweight)"""
+"""Project based on the IndominusByte FastAPI JWT Auth project"""
+
+__version__ = "0.1.0"
+
+from .auth_jwt import AuthJWT
---
+++ src/nanoid/nanoid.py
@@ -0,0 +1,28 @@
+from os import urandom
+from math import ceil, log
+
+class NanoID:
+    def __init__(self, alphabet: int = None, size: int = None, prefix: str = None):
+        self._prefix = prefix
+        self._alphabet = alphabet if alphabet else '_-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        self._size = size if size else 21
+
+    def generate(self) -> str:
+        alphabet_len = len(self._alphabet)
+        
+        mask = 1
+        if alphabet_len > 1:
+            mask = (2 << int(ceil(log(alphabet_len) / log(2)))) - 1
+        step = int(ceil(1.6 * mask * self._size / alphabet_len))
+
+        id = ''
+        while True:
+            for random_byte in bytearray(urandom(step)):
+                random_byte = random_byte & mask
+                if random_byte < alphabet_len and random_byte >= 0:
+                    id += self._alphabet[random_byte]
+                    if len(id) == self._size:
+                        if self._prefix:
+                            return f'{self._prefix}_{id}'
+                        else:
+                            return id
